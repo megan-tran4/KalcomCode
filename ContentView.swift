7853ad050e1c7ebd890b4debcd7fb8ee18cdc876
megan-tran4 <146565032+megan-tran4@users.noreply.github.com>
2023-11-02 20:00:46 -0500

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -16,168 +16,180 @@ struct ContentView: View {
     @State var food = false
     @State var cleaning = false
     @State var other = false
-    var body: some View {        
+    var body: some View {
         NavigationStack {
-            NavigationLink(destination: pendingRequests()) {
-                Text("Requests")
-                    .frame(width: 300, height: 20, alignment: .trailing)
+            ZStack {
+                ZStack {
+                    NavigationLink(destination: pendingRequests()) {
+                        Text("Requests")
+                            .font(.title3)
+                            .frame(width: 850, height: 70, alignment: .trailing)
+                    }
+                    Text("Hey, \(patient)")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .scaleEffect(x: 1.5, y: 1.5)
+                        .bold()
+                        .padding(20)
+                }
             }
-            Text("Hey, \(patient)")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .scaleEffect(x: 1.5, y: 1.5)
-                .bold()
-                .padding(10)
             VStack {
                 HStack {
                     //bathroom button
-                    ZStack {
+                    VStack{
                         if bathroom{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(.blue)
+                            Button(action: bathroomRequest){
+                                Image("toilet-blue 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
                         else{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
+                            Button(action: bathroomRequest){
+                                Image("toilet-grey 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
-                        Button(action: bathroomRequest){
-                            Label("bathroom", systemImage: "toilet")
-                                .scaleEffect(x: 2, y: 2)
-                                .labelStyle(.iconOnly)
-                                .foregroundColor(.white)
-                        }
+                        Text("assist restroom")
+                            .font(.title2)
                     }
                     .padding(20)
                     //pill button
-                    ZStack {
+                    VStack {
                         if medication{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(.blue)
+                            Button(action: medRequest){
+                                Image("medicine-blue 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
                         else{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
+                            Button(action: medRequest){
+                                Image("medicine-grey 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
-                        Button(action: medRequest){
-                            Label("medication", systemImage: "pills")
-                                .scaleEffect(x: 2, y: 2)
-                                .labelStyle(.iconOnly)
-                                .foregroundColor(.white)
-                        }
+                        Text("need medicine")
+                            .font(.title2)
                     }
                     .padding(20)
-                }
-                HStack {
-                    // water button
-                    ZStack {
+                    //water button
+                    VStack {
                         if water{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(.blue)
+                            Button(action: waterRequest){
+                                Image("water-blue 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
+                            
                         }
                         else{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
+                            Button(action: waterRequest){
+                                Image("water-grey 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
-                        Button(action: waterRequest){
-                            Label("water", systemImage: "waterbottle")
-                                .scaleEffect(x: 2, y: 2)
-                                .labelStyle(.iconOnly)
-                                .foregroundColor(.white)
-                        }
-                    }
-                    .padding(20)
-                    // food button
-                    ZStack {
-                        if food{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(.blue)
-                        }
-                        else{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
-                        }
-                        Button(action: foodRequest){
-                            Label("food", systemImage: "fork.knife")
-                                .scaleEffect(x: 2, y: 2)
-                                .labelStyle(.iconOnly)
-                                .foregroundColor(.white)
-                        }
+                        Text("need water")
+                            .font(.title2)
                     }
                     .padding(20)
                 }
                 HStack {
-                    // cleaning button
-                    ZStack {
-                        if cleaning{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(.blue)
+                    //food button
+                    VStack {
+                        if food{
+                            Button(action: foodRequest){
+                                Image("food-blue 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
                         else{
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
+                            Button(action: foodRequest){
+                                Image("food-grey 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
-                        Button(action: cleaningRequest){
-                            Label("cleaning", systemImage: "paintbrush")
-                                .scaleEffect(x: 2, y: 2)
-                                .labelStyle(.iconOnly)
-                                .foregroundColor(.white)
+                        Text("need food")
+                            .font(.title2)
+                    }
+                    .padding(20)
+                    // cleaning button
+                    VStack {
+                        if cleaning{
+                            Button(action: cleaningRequest){
+                                Image("trash-blue 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
                         }
+                        else{
+                            Button(action: cleaningRequest){
+                                Image("trash-grey 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height:140)
+                            }
+                        }
+                        Text("need cleaning")
+                            .font(.title2)
                     }
                     .padding(20)
                     //text nav link
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 5)
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
+                    VStack{
                         NavigationLink(destination: textOption()){
-                            Label("text request", systemImage: "ellipsis.circle")
-                                .scaleEffect(x: 2, y: 2)
-                                .labelStyle(.iconOnly)
-                                .foregroundColor(.white)
+                            Image("nurse-grey 180x180")
+                                .scaleEffect(0.8)
+                                .frame(width: 140, height: 140)
                         }
+                        Text("ask a nurse")
+                            .font(.title2)
                     }
-                        .padding(20)
+                    .padding(20)
                 }
                 //request sending button
                 NavigationLink {
                     if (bathroom || medication || water || food || cleaning){
-                        requestRecieved()
+                            requestRecieved()
                     }
                 } label: {
                     if (bathroom || medication || water || food || cleaning){
                         ZStack {
                             RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 250, height: 75)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                .frame(width: 300, height: 70)
+                                .foregroundColor(Color(red:74/255, green:154/255, blue:212/255))
                             Text("Request")
                                 .font(.title)
                                 .foregroundColor(.white)
                         }
+                        .padding(20)
                     }
                     else {
                         ZStack {
                             RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 250, height: 75)
-                                .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
+                                .frame(width: 300, height: 70)
+                                .foregroundColor(Color(red: 141/255, green: 203/255, blue: 239/255))
                             Text("Request")
                                 .font(.title)
                                 .foregroundColor(.white)
                         }
+                        .padding(20)
                     }
                 }
-
-
             }
+            
         }
+        ZStack{
+            Color.gray
+                .frame(height:70)
+            Image("emergency-red 180x180")
+                .resizable()
+                .frame(width:50, height:50)
+            
+        }
+        .ignoresSafeArea()
     }
     func bathroomRequest() {
         bathroom = !bathroom
@@ -201,39 +213,6 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-}
-
-
-struct buttons: View {
-    //var change: String
-    @State var click: Bool = false
-    @State var chosen: Color = .white
-    var request: String
-    var icon: String
-    var body: some View {
-        ZStack {
-            if click{
-                RoundedRectangle(cornerRadius: 5)
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.blue)
-            }
-            else{
-                RoundedRectangle(cornerRadius: 5)
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(Color(red: 119/255, green: 190/255, blue: 248/255))
-            }
-            Button(action: actionRequest){
-                Label(request, systemImage: icon)
-                    .scaleEffect(x: 2, y: 2)
-                    .labelStyle(.iconOnly)
-                    .foregroundColor(.white)
-            }
-        }
-        
-    }
-    func actionRequest() {
-        click = !click
-    }
 }
 
 
