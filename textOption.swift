8d91ac27c0feae8ad91bf516c91f2cd01fb4ba52
megan-tran4 <146565032+megan-tran4@users.noreply.github.com>
2023-11-02 20:02:35 -0500

    Update textOption.swift

--- textOption.swift
+++ textOption.swift
@@ -12,28 +12,27 @@ struct textOption: View {
     var body: some View {
         //Spacer()
         ZStack {
-            Color(red: 119/255, green: 190/255, blue: 248/255)
+            Color(Color(red: 141/255, green: 203/255, blue: 239/255))
             VStack {
                 Text("Type your needs below:")
-                    .frame(width: 320, alignment: .leading)
-                    .font(.title2)
+                    .frame(width: 1050, alignment: .leading)
+                    .font(.title)
                     .bold()
                 
                 ZStack {
                     RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 320, height: 50)
+                        .frame(width: 1050, height: 50)
                         .foregroundColor(.white)
                         .border(.black)
                     TextField("What do you need assitance with?", text: $request)
-                        .frame(width: 300, alignment: .leading)
-                    //.frame(width: 300, height: 400, alignment: .leading)
+                        .frame(width: 1030, alignment: .leading)
                         .foregroundColor(.black)
                 }
                 NavigationLink(destination: requestRecieved()){
                     ZStack {
                         RoundedRectangle(cornerRadius: 5)
-                            .frame(width: 250, height: 75)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .frame(width: 300, height: 70)
+                            .foregroundColor(Color(red:74/255, green:154/255, blue:212/255))
                         Text("Request")
                             .font(.title)
                             .foregroundColor(.white)
@@ -42,6 +41,7 @@ struct textOption: View {
                 .padding(20)
             }
         }
+        .ignoresSafeArea()
     }
 }
 
